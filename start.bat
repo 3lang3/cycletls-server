94622ede21121466b3975221079736be357438bd
3lang3 <675483520@qq.com>
2024-07-20 14:34:10 +0800

    chore: update

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-call npm install -g tyarn
+call npm i -g yarn tyarn
 call tyarn
 call npm start
--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 if ! command -v tyarn &> /dev/null
 then
     echo "安装tyarn..."
-    npm i -g tyarn
+    npm i -g yarn tyarn
 fi
 
 # 安装根目录下的依赖
